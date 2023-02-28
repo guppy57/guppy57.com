d72d2cf875420bdfc9c72565cc1594003f90e9dc
Armaan Gupta <armaan@guppy.im>
2023-02-28 00:09:52 -0600

    feat: added label to simple layout

--- src/components/SimpleLayout.jsx
+++ src/components/SimpleLayout.jsx
@@ -1,9 +1,12 @@
 import { Container } from '@/components/Container'
 
-export function SimpleLayout({ title, intro, children }) {
+export function SimpleLayout({ label, title, intro, children }) {
   return (
     <Container className="mt-16 sm:mt-32">
       <header className="max-w-2xl">
+        <div className='mb-6'>
+          <span className="text-sm font-normal bg-purple-100 py-2 px-4 rounded-2xl text-purple-600">{label}</span>
+        </div>
         <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
           {title}
         </h1>
