36c1c6f8014571f373b1d3e0df82b21519bdec89
Armaan Gupta <armaan@guppy.im>
2023-03-01 01:36:44 -0600

    ref: removed twitter

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -10,7 +10,6 @@ import {
   GitHubIcon,
   InstagramIcon,
   LinkedInIcon,
-  TwitterIcon,
 } from '@/components/SocialIcons';
 import logoAirbnb from '@/images/logos/airbnb.svg';
 import logoFacebook from '@/images/logos/facebook.svg';
@@ -261,38 +260,36 @@ export default function Home({ articles }) {
         </title>
         <meta
           name="description"
-          content="I’m Spencer, a software designer and entrepreneur based in New York City. I’m the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
+          content="I&apos;m Armaan, a software engineer, designer and entrepreneur based in Ames, Iowa."
         />
       </Head>
       <Container className="mt-9">
         <div className="max-w-2xl">
           <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            Software designer, founder, and amateur astronaut.
+            Software engineer and designer to aid the human condition.
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Spencer, a software designer and entrepreneur based in New York
-            City. I’m the founder and CEO of Planetaria, where we develop
-            technologies that empower regular people to explore space on their
-            own terms.
+            I’m Armaan, a software engineer, designer and entrepreneur based in
+            Ames, Iowa. I&apos;m currently studying Management Information
+            Systems at Iowa State. When I&apos;m skipping class, it&apos;s
+            usually to work on my company, Kreative, where I&apos;m hacking
+            together products and services to make people&apos;s lives a tad bit
+            easier. Or, I&apos;m probably catching up on the latest TV show
+            I&apos;m binging.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
-              href="https://twitter.com"
-              aria-label="Follow on Twitter"
-              icon={TwitterIcon}
-            />
-            <SocialLink
-              href="https://instagram.com"
+              href="https://instagram.com/armaangupta57"
               aria-label="Follow on Instagram"
               icon={InstagramIcon}
             />
             <SocialLink
-              href="https://github.com"
+              href="https://github.com/guppy57"
               aria-label="Follow on GitHub"
               icon={GitHubIcon}
             />
             <SocialLink
-              href="https://linkedin.com"
+              href="https://linkedin.com/in/armaangupta57"
               aria-label="Follow on LinkedIn"
               icon={LinkedInIcon}
             />
