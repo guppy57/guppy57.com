949cf6c4c1df66bf9c85a786c47b8b5f186a233b
Armaan Gupta <armaan@guppy.im>
2023-03-04 06:38:15 -0600

    ref: moved project data

--- src/pages/projects.jsx
+++ src/pages/projects.jsx
@@ -3,56 +3,8 @@ import Image from 'next/image';
 
 import { Card } from '@/components/Card';
 import { SimpleLayout } from '@/components/SimpleLayout';
-import logoPokedex from '@/images/logos/pokedex.png';
-import logoKreativeID from '@/images/logos/id.png';
-import logoKreativeHyperlink from '@/images/logos/hyperlink.png';
-
-const projects = [
-  {
-    name: 'Kreative ID',
-    description:
-      'Cross-service identity authentication and authorization service for Kreative.',
-    link: {
-      href: 'https://github.com/kreative/id-api',
-      label: 'github.com',
-    },
-    logo: logoKreativeID,
-    skills: ['TypeScript', 'Nest.js', 'React', 'SQL'],
-  },
-  {
-    name: 'Kreative Hyperlink',
-    description:
-      'Blazing-fast and free URL shorterner with analytics, leaderboards, and more.',
-    link: {
-      href: 'https://kreativehyperlink.com',
-      label: 'kreativehyperlink.com',
-    },
-    logo: logoKreativeHyperlink,
-    skills: ['Nest.js', 'TypeScript', 'React', 'Next,js'],
-  },
-  {
-    name: "Armaan's Pokedex",
-    description:
-      'A simple Pokedex app built with Next.js and PokeAPI, just for fun.',
-    link: {
-      href: 'https://pokedex.kreativeusa.com',
-      label: 'pokedex.kreativeusa.com',
-    },
-    logo: logoPokedex,
-    skills: ['TypeScript', 'Next.js', 'PostgreSQL'],
-  },
-];
-
-function LinkIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        d="M15.712 11.823a.75.75 0 1 0 1.06 1.06l-1.06-1.06Zm-4.95 1.768a.75.75 0 0 0 1.06-1.06l-1.06 1.06Zm-2.475-1.414a.75.75 0 1 0-1.06-1.06l1.06 1.06Zm4.95-1.768a.75.75 0 1 0-1.06 1.06l1.06-1.06Zm3.359.53-.884.884 1.06 1.06.885-.883-1.061-1.06Zm-4.95-2.12 1.414-1.415L12 6.344l-1.415 1.413 1.061 1.061Zm0 3.535a2.5 2.5 0 0 1 0-3.536l-1.06-1.06a4 4 0 0 0 0 5.656l1.06-1.06Zm4.95-4.95a2.5 2.5 0 0 1 0 3.535L17.656 12a4 4 0 0 0 0-5.657l-1.06 1.06Zm1.06-1.06a4 4 0 0 0-5.656 0l1.06 1.06a2.5 2.5 0 0 1 3.536 0l1.06-1.06Zm-7.07 7.07.176.177 1.06-1.06-.176-.177-1.06 1.06Zm-3.183-.353.884-.884-1.06-1.06-.884.883 1.06 1.06Zm4.95 2.121-1.414 1.414 1.06 1.06 1.415-1.413-1.06-1.061Zm0-3.536a2.5 2.5 0 0 1 0 3.536l1.06 1.06a4 4 0 0 0 0-5.656l-1.06 1.06Zm-4.95 4.95a2.5 2.5 0 0 1 0-3.535L6.344 12a4 4 0 0 0 0 5.656l1.06-1.06Zm-1.06 1.06a4 4 0 0 0 5.657 0l-1.061-1.06a2.5 2.5 0 0 1-3.535 0l-1.061 1.06Zm7.07-7.07-.176-.177-1.06 1.06.176.178 1.06-1.061Z"
-        fill="currentColor"
-      />
-    </svg>
-  );
-}
+import projects from '@/lib/projects';
+import { LinkIcon } from '@/components/LinkIcon';
 
 export default function Projects() {
   return (
