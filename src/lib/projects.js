86976d96a680498b24e60521c292692aadeadecb
Armaan Gupta <armaan@guppy.im>
2023-04-03 09:47:02 -0500

    feat: added kreative templates project

--- src/lib/projects.js
+++ src/lib/projects.js
@@ -1,6 +1,7 @@
 import logoPokedex from '@/images/logos/pokedex.png';
 import logoKreativeID from '@/images/logos/id.png';
 import logoKreativeHyperlink from '@/images/logos/hyperlink.png';
+import logoKTemplates from '@/images/ktemplates.png';
 
 const projects = [
   {
@@ -13,12 +14,12 @@ const projects = [
     },
     logo: logoKreativeID,
     skills: ['TypeScript', 'Nest.js', 'React', 'SQL'],
-    featured: true,
+    featured: false,
   },
   {
     name: 'Kreative Hyperlink',
     description:
-      'Blazing-fast and free URL shorterner with analytics, leaderboards, and more.',
+      'Blazing-fast and free URL shortening service with analytics, leaderboards, and more.',
     link: {
       href: 'https://kreativehyperlink.com',
       label: 'kreativehyperlink.com',
@@ -36,8 +37,20 @@ const projects = [
       label: 'pokedex.kreativeusa.com',
     },
     logo: logoPokedex,
-    skills: ['TypeScript', 'Next.js', 'PostgreSQL'],
+    skills: ['TypeScript',
+      'Next.js', 'PostgreSQL'],
     featured: false,
+  },
+  {
+    name: "Kreative Templates",
+    description: 'Peer to peer marketplace to share Obsidian templates.',
+    link: {
+      href: 'https://kreativetemplates.co',
+      label: 'kreativetemplates.co',
+    },
+    logo: logoKTemplates,
+    skills: ['TypeScript', 'Next.js', 'Nest.js', 'PostgreSQL'],
+    featured: true,
   },
 ];
 
