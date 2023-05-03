6a2c7477f70ff69ff7eac2f5ee20117ee54bf014
Armaan Gupta <armaan@guppy.im>
2023-05-02 21:40:48 -0500

    ref: thank you is now contact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,9 +1,22 @@
 import Head from 'next/head';
 
+import { SimpleLayout } from '@/components/SimpleLayout';
+
 export default function Contact() {
   return (
     <>
-      
+      <Head>
+        <title>Drop a line - Armaan Gupta</title>
+        <meta
+          name="description"
+          content="Contact me and drop a line, I'll be in touch!"
+        />
+      </Head>
+      <SimpleLayout
+        label="Contact me"
+        title="Drop me a line"
+        intro={null}
+      />
     </>
-  )
+  );
 }
